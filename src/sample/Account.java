e1bccd6f6f7661ca378caed200de9eb16cb45197
Samuel <nkg17tru@uea.ac.uk>
2020-03-09 14:53:12 +0000

    added to account class

--- src/sample/Account.java
+++ src/sample/Account.java
@@ -6,11 +6,69 @@ public class Account {
     String username;
     String email;
     String password;
-    int detailsID;
+    String name;
+    String surname;
+    //FitnessRegime fitnessRegime;
+    enum Sex{MALE, FEMALE};
+    //goals Goal
+    //diet Diet
+    AccountDetails details;
     //UserDetails class with weight height profile pic etc
 
-    public Account(){
+    public Account(String username, String email, String password, String name, String surname, AccountDetails details) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.details = details;
     }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public AccountDetails getDetails() {
+        return details;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setDetails(AccountDetails details) {
+        this.details = details;
+    }
 }
--- src/sample/AccountDetails.java
+++ src/sample/AccountDetails.java
@@ -3,5 +3,47 @@ package sample;
 public class AccountDetails {
     //fields
     int detailsID;
+    int height;
+    int weight;
+    Account aAccount;
 
+
+    public AccountDetails(int detailsID, int height, int weight, Account aAccount) {
+        this.detailsID = detailsID;
+        this.height = height;
+        this.weight = weight;
+        this.aAccount = aAccount;
+    }
+
+    public int getDetailsID() {
+        return detailsID;
+    }
+
+    public void setDetailsID(int detailsID) {
+        this.detailsID = detailsID;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Account getaAccount() {
+        return aAccount;
+    }
+
+    public void setaAccount(Account aAccount) {
+        this.aAccount = aAccount;
+    }
 }
