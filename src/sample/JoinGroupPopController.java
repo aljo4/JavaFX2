03921276008e3ac5e83ee6652b087a4fe7fbe4f0
Lawrence <vpx17epu@uea.ac.uk>
2020-03-20 12:07:12 +0000

    controller for join group pop up

---
+++ src/sample/JoinGroupPopController.java
@@ -0,0 +1,65 @@
+package sample;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import com.jfoenix.controls.JFXListView;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.SelectionMode;
+
+import javax.imageio.IIOException;
+import javax.swing.*;
+public class JoinGroupPopController {
+
+    @FXML JFXButton cancelJoinGroup;
+    @FXML JFXButton joinGroup;
+    @FXML JFXTextField joinGroupName;
+    @FXML JFXPasswordField joinGroupPass;
+    @FXML Label notify;
+
+    public void closeJoinPop(ActionEvent ae){
+        Stage w = (Stage) ((Node) ae.getSource()).getScene().getWindow();
+        w.close();
+    }
+
+    public void joinGroupButton(ActionEvent ae){
+        String groupName = joinGroupName.getText();
+        String groupPass = joinGroupPass.getText();
+        try {
+            Group g = Group.findGroup(groupName);
+            System.out.println(g);
+            if (g!=null) {
+                System.out.println(g.getGroupPass());
+                if (groupPass.equals(g.groupPass)) {
+                    System.out.println("1");
+                    g.addMember(LoginController.currentAccount.getUsername());
+                    System.out.println("2");
+                    LoginController.currentAccount.groupList.add(groupName);
+                    System.out.println("3");
+                    notify.setText("Success!");
+                    Stage w = (Stage) ((Node) ae.getSource()).getScene().getWindow();
+                    w.close();
+                }
+                else
+                    notify.setText("Wrong password");
+            }
+            else
+                notify.setText("No group with that name");
+        }
+        catch (Exception ioex){
+            System.out.println(":(");
+        }
+
+
+    }
+}
