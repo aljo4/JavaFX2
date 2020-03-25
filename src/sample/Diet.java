8ad165311f4b253f5551a85b4cf6ac6d816dfcca
Samuel <nkg17tru@uea.ac.uk>
2020-03-25 14:02:25 +0000

    controller for login complete need to connect it to next screen

---
+++ src/sample/Diet.java
@@ -0,0 +1,82 @@
+package sample;
+
+
+
+public class Diet {
+    public enum mealType{
+        BREAKFAST,
+        LUNCH,
+        DINNER;
+
+        mealType() {
+        }
+    }
+
+
+    private mealType mealtype;
+    private FoodType foods;
+    private DrinkType drinks;
+    private int portionSize; //will this be used?
+    private int caloricIntake; //will this be used?
+
+
+    public Diet() {
+        this.foods = foods;
+        this.drinks = drinks;
+        this.portionSize = portionSize;
+        this.caloricIntake = caloricIntake;
+        this.mealtype = mealtype;
+    }
+
+
+    public FoodType getFoods() {
+        return foods;
+    }
+
+    public mealType getMealtype() {
+        return mealtype;
+    }
+
+    public void setMealtype(mealType mealtype) {
+        this.mealtype = mealtype;
+    }
+
+    public void setDrinks(DrinkType drinks) {
+        this.drinks = drinks;
+    }
+
+    public DrinkType getDrinks() {
+        return drinks;
+    }
+
+    public void setFoods(FoodType foods) {
+        this.foods = foods;
+    }
+
+    public int getPortionSize() {
+        return portionSize;
+    }
+
+    public void setPortionSize(int portionSize) {
+        this.portionSize = portionSize;
+    }
+
+    public int getCaloricIntake() {
+        return caloricIntake;
+    }
+
+    public void setCaloricIntake(int caloricIntake) {
+        this.caloricIntake = caloricIntake;
+        foods.getFood().setCalories(caloricIntake);
+        drinks.getDrink().setCalories(caloricIntake);
+
+    }
+
+    public void calculateCaloriesLeftForDay(){
+
+    }
+
+    public void calculateDailyCaloricIntake(){}
+
+
+}
