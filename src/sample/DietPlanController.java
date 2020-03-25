722275849ef762aa9d7098e49167ac461cdfcf42
ohkjess <dqw18jvu@uea.ac.uk>
2020-03-25 15:59:20 +0000

    create diet page

--- src/sample/DietPlanController.java
+++ src/sample/DietPlanController.java
@@ -2,42 +2,74 @@ package sample;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
 
 import java.awt.event.ActionEvent;
 import java.io.*;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.Date;
-import java.util.*;
 
-public class DietPlanController implements Initializable {
-    @FXML private ChoiceBox<FoodType.Foods> food;
-    ObservableList<FoodType.Foods> foodChoice = FXCollections.observableArrayList(FoodType.Foods.values());
+public class DietPlanController<JFXTextField> {
+
+    //all fields on page
+
     @FXML
-    private ChoiceBox<DrinkType.Drinks> drink;
-    ObservableList<DrinkType.Drinks> drinkChoice = FXCollections.observableArrayList(DrinkType.Drinks.values());
+    private JFXTextField DietName;
+    @FXML
+    private ChoiceBox DietType;
+    @FXML
+    private DatePicker StartDate;
+    @FXML
+    private DatePicker FinishDate;
+    @FXML
+    private JFXTextField CaloriesLimit;
+    @FXML
+    private JFXButton SaveDiet;
 
-
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        food.setItems(foodChoice);
-        drink.setItems(drinkChoice);
+    @FXML
+    public void initialize() {
+        DietType.getItems().add("Paleo");
+        DietType.getItems().add("Vegan");
+        DietType.getItems().add("Low-Carb");
+        DietType.getItems().add("Dukan");
+        DietType.getItems().add("Ultra Low Fat");
+        DietType.getItems().add("Atkins");
+        DietType.getItems().add("HCG");
+        DietType.getItems().add("Zone");
+        DietType.getItems().add("Intermittent Fasting");
     }
 
-    
+
+    //save to CSV
+
+
+    //WRITE TO CSV
+
+
+//    public void pickDiet(ActionEvent ae) throws Exception {
+//
+//    }
+//
+//    public static boolean pickDate(Date t) {
+//        if (t == null || t.trim().isEmpty()) {
+//            System.out.println("Please fill in date");
+//            return false;
+//        }
+//
+//        Date StartDate = s
+//        if (t < s || s.trim().isEarlier) {
+//            System.out.println("Please pick later date")
+//            retrun false;
+//        } else
+//            return true;
+//    }
 }
 
 
