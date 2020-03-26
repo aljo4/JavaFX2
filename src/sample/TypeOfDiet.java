63bd4807984553a7a93c13dafb4d010935c0cc8d
ohkjess <dqw18jvu@uea.ac.uk>
2020-03-26 15:16:00 +0000

    create diet page

--- src/sample/TypeOfDiet.java
+++ src/sample/TypeOfDiet.java
@@ -1,22 +1,30 @@
 package sample;
 
 
+import jdk.jfr.Description;
+
 public class TypeOfDiet{
     public enum Diets {
-        Paleo(0),
-        Vegan(0),
-        LowCarb(0),
-        Dukan(0),
-        UltraLowFat(0),
-        Atkins(0),
-        HCG(0),
-        Zone(0),
-        IntermittentFasting(0);
+        Paleo(1200,"no bread,no sugar, not milk, no rice, no corn, no beans, no potatoes"),
+        Vegan(1500,"no meat,no chicken, no fish, no dairy, no animal based products"),
+        LowCarb(1800,"no bread, no oils, no fruit, no juice, no fizzy drinks, no fats, no pasta"),
+        Dukan(1000,"no red meat, no dairy, no processed foods, no refined sugars, no fats"),
+        UltraLowFat(1800,"mainly plant-based foods, reduce intake of animal products"),
+        Atkins(1800,"no sugar, no grains, no vegetable oils"),
+        HCG(500,"no butter, no oils,no sugar"),
+        Zone(1500,"no processed grains, no potatoes, no egg yolks, no organ meets, no soda"),
+        IntermittentFasting(600,"eating full meals every other day"),
+        NoDiet(0,"N/A");
+
 
         private int calories;
 
-        Diets(int calories) {
+        private String description;
+
+
+        Diets(int calories,String description) {
             this.calories = calories;
+            this.description =description;
         }
 
         public int getCalories() {
@@ -38,6 +46,7 @@ public class TypeOfDiet{
     public void setDiet(TypeOfDiet.Diets diet) {
         this.diet = diet;
     }
+
 
 
 
