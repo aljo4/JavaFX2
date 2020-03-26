b1ce6572f3c917c53bfc6d94eaa5bd999903d5d4
Lawrence <vpx17epu@uea.ac.uk>
2020-03-26 16:13:22 +0000

    sfg

--- src/sample/TypeOfDiet.java
+++ src/sample/TypeOfDiet.java
@@ -5,6 +5,7 @@ import jdk.jfr.Description;
 
 public class TypeOfDiet{
     public enum Diets {
+        NoDiet(0,"N/A"),
         Paleo(1200,"no bread,no sugar, not milk, no rice, no corn, no beans, no potatoes"),
         Vegan(1500,"no meat,no chicken, no fish, no dairy, no animal based products"),
         LowCarb(1800,"no bread, no oils, no fruit, no juice, no fizzy drinks, no fats, no pasta"),
@@ -13,8 +14,8 @@ public class TypeOfDiet{
         Atkins(1800,"no sugar, no grains, no vegetable oils"),
         HCG(500,"no butter, no oils,no sugar"),
         Zone(1500,"no processed grains, no potatoes, no egg yolks, no organ meets, no soda"),
-        IntermittentFasting(600,"eating full meals every other day"),
-        NoDiet(0,"N/A");
+        IntermittentFasting(600,"eating full meals every other day");
+
 
 
         private int calories;
@@ -22,7 +23,7 @@ public class TypeOfDiet{
         private String description;
 
 
-        Diets(int calories,String description) {
+        Diets(int calories,String description){
             this.calories = calories;
             this.description =description;
         }
@@ -37,7 +38,9 @@ public class TypeOfDiet{
     }
     private Diets diet;
 
-    public TypeOfDiet(Diets diet){this.diet= diet;}
+    public TypeOfDiet(Diets diet){
+        this.diet= diet;
+    }
 
     public TypeOfDiet.Diets getDiet() {
         return diet;
