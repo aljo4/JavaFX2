883c45fd6075711ca805e8f9ea918daace603e9c
ohkjess <dqw18jvu@uea.ac.uk>
2020-03-26 13:16:25 +0000

    create diet page

---
+++ src/sample/TypeOfDiet.java
@@ -0,0 +1,17 @@
+package sample;
+
+import com.jfoenix.responsive.JFXResponsiveHandler;
+
+public class TypeOfDiet {
+    public enum Diets{
+        Paleo,
+        Vegan,
+        LowCarb,
+        Dukan,
+        UltraLowFat,
+        Atkins,
+        HCG,
+        Zone,
+        IntermittentFasting;
+        }
+}
