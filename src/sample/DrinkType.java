902d8ce030bf9fd2644855d7ee75cca6311bbe05
Samuel <nkg17tru@uea.ac.uk>
2020-03-31 13:04:57 +0100

    nearly done

--- src/sample/DrinkType.java
+++ src/sample/DrinkType.java
@@ -1,46 +1,33 @@
 package sample;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class DrinkType {
-     public enum Drinks{
-         WATER(0),
-         COFFEE(0),
-         TEA(0),
-         ALCOHOL(0),
-         SOFTDRINKS(0),
-         JUICES(0),
-         ENERGYDRINKS(0);
+    private ArrayList<String>Drinks;
 
-         private int calories;
+    public DrinkType(){
+        Drinks = new ArrayList<String>(7);
+        Drinks.add("Water");
+        Drinks.add("Coffee");
+        Drinks.add("Tea");
+        Drinks.add("Alcohol");
+        Drinks.add("Soft Drinks");
+        Drinks.add("Juices");
+        Drinks.add("Energy Drinks");
 
-         Drinks(int calories){
-             this.calories = calories;
-         }
 
-         public int getCalories() {
-             return calories;
-         }
-
-         public void setCalories(int calories) {
-             this.calories = calories;
-         }
-     }
-     private Drinks drink;
-
-    public DrinkType(Drinks drink) {
-        this.drink = drink;
     }
 
-    public Drinks getDrink() {
-        return drink;
+    public ArrayList<String> getDrinks() {
+        return Drinks;
     }
 
-    public void setDrink(Drinks drink) {
-        this.drink = drink;
+    public void setDrinks(ArrayList<String> drinks) {
+        Drinks = drinks;
     }
 
-    public static void main(String[] args) {
-        DrinkType aDrink = new DrinkType(Drinks.WATER);
-        aDrink.getDrink().WATER.setCalories(23);//water has already been assigned
-        System.out.println(aDrink.getDrink().WATER.getCalories());
+    public void addToDrinks(String drink){
+        Drinks.add(drink);
     }
 }
