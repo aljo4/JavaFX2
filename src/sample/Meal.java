3e94cfb9186efbb484fb5ef89680cc841f51749d
Lawrence <vpx17epu@uea.ac.uk>
2020-04-01 03:24:53 +0100

    meal update

--- src/sample/Meal.java
+++ src/sample/Meal.java
@@ -1,32 +1,48 @@
 package sample;
 
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Meal {
     public enum mealType{
         BREAKFAST,
         LUNCH,
-        DINNER;
+        DINNER,
+        SNACK;
         mealType() {
         }
     }
     private mealType mealtype;
-    private FoodType foods;
-    private DrinkType drinks;
-    private int portionSize; //will this be used?
+    private ArrayList<Edible> food;
     private int caloricIntake; //will this be used?
+    private LocalDate mealDate;
 
     public Meal() {
-        this.foods = foods;
-        this.drinks = drinks;
-        this.portionSize = portionSize;
+        this.food = food;
         this.caloricIntake = caloricIntake;
         this.mealtype = mealtype;
     }
 
+    public Meal(mealType mealtype, ArrayList<Edible> food){
+        this.mealtype = mealtype;
+        this.food = food;
+        this.mealDate = LocalDate.now();
+        int cals = 0;
+        for (Edible e:food){
+            cals+=e.getCalCount();
+        }
+        this.caloricIntake = cals;
+    }
 
-    public FoodType getFoods() {
-        return foods;
+
+    public ArrayList<Edible> getFood() {
+        return food;
+    }
+
+    public LocalDate getMealDate() {
+        return mealDate;
     }
 
     public mealType getMealtype() {
@@ -37,40 +53,34 @@ public class Meal {
         this.mealtype = mealtype;
     }
 
-    public void setDrinks(DrinkType drinks) {
-        this.drinks = drinks;
-    }
-
-    public DrinkType getDrinks() {
-        return drinks;
-    }
-
-    public void setFoods(FoodType foods) {
-        this.foods = foods;
-    }
-
-    public int getPortionSize() {
-        return portionSize;
-    }
-
-    public void setPortionSize(int portionSize) {
-        this.portionSize = portionSize;
-    }
-
     public int getCaloricIntake() {
         return caloricIntake;
     }
 
     public void setCaloricIntake(int caloricIntake) {
         this.caloricIntake = caloricIntake;
+    }
 
+    public void setFood(ArrayList<Edible> food) {
+        this.food = food;
+    }
+
+    public void setMealDate(LocalDate mealDate) {
+        this.mealDate = mealDate;
     }
 
     public void calculateCaloriesLeftForDay(){
-
     }
 
-    public void calculateDailyCaloricIntake(){}
+    public void calculateDailyCaloricIntake(){
+    }
 
+    @Override
+    public String toString() {
+        return "some text here";
+    }
 
+    public String toStringForMeals(){
+        return "Type of Meal: " + mealtype + "\n"+ "Total Calories: " + caloricIntake;
+    }
 }
