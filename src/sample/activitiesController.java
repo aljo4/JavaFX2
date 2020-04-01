f4471d9c5a7e303e5ca66f2ee38e48bedffdcf33
Samuel <nkg17tru@uea.ac.uk>
2020-04-01 13:11:58 +0100

    nearly done

--- src/sample/activitiesController.java
+++ src/sample/activitiesController.java
@@ -23,16 +23,33 @@ import java.time.LocalDate;
 import java.util.*;
 
 public class activitiesController implements Initializable {
+    public activitiesController() {
+
+    }
+
     @FXML
-    private ComboBox<Activities> activity;
-    ObservableList<Activities> exerciseChoice = FXCollections.observableArrayList(Activities.values());
+    private DatePicker activityDate;
+    @FXML
+    private ComboBox<Activity.Activities> activity;
+    ObservableList<Activity.Activities> exerciseChoice = FXCollections.observableArrayList(Activity.Activities.values());
     @FXML
     private TextField duration;
 
     @FXML
     private TextField reps;
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        activityDate.setValue(LocalDate.now());
+        activityDate.setDayCellFactory(datePicker1 -> new DateCell() {
+                    public void updateItem(LocalDate date, boolean empty) {
+                        super.updateItem(date, empty);
+                        LocalDate today = LocalDate.now();
+
+                        setDisable(empty || date.compareTo(today) < 0);
+                    }
+                }
+
+        );
         activity.setItems(exerciseChoice);
     }
 
@@ -44,11 +61,24 @@ public class activitiesController implements Initializable {
             alert.setContentText("Empty fields, please try again");
             alert.showAndWait();
         }
+        Activity activity1 = new Activity(activity.getSelectionModel().getSelectedItem(),activityDate.getValue());
         Account.getInstance().setActivities(activity.getSelectionModel().getSelectedItem());
         Account.getInstance().getActivities().setDuration(Integer.parseInt(duration.getText()));
         Account.getInstance().getActivities().setRepetitions(Integer.parseInt(reps.getText()));
         Account.getInstance().getExercises().add(Account.getInstance().getActivities());
-        Account.getInstance().getAccountLists().saveActivityToFile(Account.getInstance().getActivities());
+        System.out.println(activity1.getActivitiesEnum());
+        activity1.getActivitiesEnum().setDuration(Integer.parseInt(duration.getText()));
+        activity1.getActivitiesEnum().setRepetitions(Integer.parseInt(reps.getText()));
+        Account.getInstance().getAccountLists().saveActivityToFile(activity1);
+
+
+
+//        activity.getSelectionModel().getSelectedItem();
+//        System.out.println(activity.getSelectionModel().getSelectedItem());
+////        Account.getInstance().getActivities().getActivitiesEnum().setDuration(Integer.parseInt(duration.getText()));
+////        Account.getInstance().getActivities().getActivitiesEnum().setRepetitions(Integer.parseInt(reps.getText()));
+////        Account.getInstance().getExercises().add(Account.getInstance().getActivities().getActivitiesEnum());
+////        Account.getInstance().getAccountLists().saveActivityToFile(Account.getInstance().getActivities());
 
     }
 }
